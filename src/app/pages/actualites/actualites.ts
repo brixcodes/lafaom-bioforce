848b4cb12de56ca4d067313a727a305a84ff71eb
lafaom <nanyangbrice.devops@gmail.com>
2025-10-12 14:02:03 +0100

    re-oder

--- src/app/pages/actualites/actualites.ts
+++ src/app/pages/actualites/actualites.ts
@@ -3,11 +3,12 @@ import { Header } from '../../components/actualite/header/header';
 import { Section1 } from '../../components/actualite/section-1/section-1';
 import { Section2 } from '../../components/actualite/section-2/section-2';
 import { Section3 } from '../../components/actualite/section-3/section-3';
+import { Section6 } from '../../components/acceuil/section-6/section-6';
 
 @Component({
   selector: 'app-actualites',
   standalone: true,
-  imports: [Header, Section1, Section2, Section3],
+  imports: [Header, Section1, Section2, Section3, Section6],
   templateUrl: './actualites.html',
   styleUrl: './actualites.css'
 })
