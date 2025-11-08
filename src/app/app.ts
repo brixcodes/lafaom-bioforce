25ff42155b111ce91662849a7f0c895425d8277c
kseme277 <kseme277@gmail.com>
2025-11-08 21:42:06 +0100

    aaaaaa

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,13 @@ import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footers } from './components/footers/footers';
+import { WhatsAppButton } from './components/whatsapp-button/whatsapp-button';
 import { SimpleTranslateService } from './services/simple-translate.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, Footers],
+  imports: [RouterOutlet, Header, Footers, WhatsAppButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
